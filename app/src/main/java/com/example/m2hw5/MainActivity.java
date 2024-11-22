4ba97946152adfb4a9f5f5742f8742312110a11a
Liliboba <toperatai@gmail.com>
2024-11-22 18:43:23 +0600

    HW6

--- app/src/main/java/com/example/m2hw5/MainActivity.java
+++ app/src/main/java/com/example/m2hw5/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.m2hw5;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -145,6 +146,16 @@ public class MainActivity extends AppCompatActivity {
                }
 
            }
+           Button activity = findViewById(R.id.activity_btr);
+           activity.setAlpha(1);
        }
+    }
+
+    public void clickOnBTRActivity(View view) {
+        String text = textView.getText().toString();
+Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+intent.putExtra("key1", text);
+startActivity(intent);
+onDestroy();
     }
 }
